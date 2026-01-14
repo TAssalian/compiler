eac5ccf788b31b2ab6c921ae7739b2b8ac09dfe6
Thomas A. <tom.assalian@gmail.com>
2026-01-14 13:40:48 -0500

    Majority of Token class complete, missing error types and output formats

---
+++ tokens.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass
+from enum import Enum
+
+class TokenType(Enum):
+    ID = "id"
+    INTNUM = "intnum"
+    FLOATNUM = "floatnum"
+
+    EQ = "eq"             
+    NOTEQ = "noteq"        
+    LT = "lt"             
+    GT = "gt"             
+    LEQ = "leq"            
+    GEQ = "geq"            
+    ASSIGN = "assign"  
+
+    PLUS = "plus"        
+    MINUS = "minus"   
+    MULT = "mult"    
+    DIV = "div"     
+
+    OPENPAR = "openpar" 
+    CLOSEPAR = "closepar"  
+    OPENCUBR = "opencubr"  
+    CLOSECUBR = "closecubr" 
+    OPENSQBR = "opensqbr" 
+    CLOSESQBR = "closesqbr" 
+    SEMI = "semi"    
+    COMMA = "comma"      
+    DOT = "dot"     
+    COLON = "colon"      
+    COLONCOLON = "coloncolon"
+
+    IF = "if"
+    THEN = "then"
+    ELSE = "else"
+    WHILE = "while"
+    CLASS = "class"
+    INTEGER = "integer"
+    FLOAT = "float"
+    DO = "do"
+    END = "end"
+    PUBLIC = "public"
+    PRIVATE = "private"
+    OR = "or"
+    AND = "and"
+    NOT = "not"
+    READ = "read"
+    WRITE = "write"
+    RETURN = "return"
+    INHERITS = "inherits"
+    LOCAL = "local"
+    VOID = "void"
+    MAIN = "main"
+    
+    BLOCKCMT = "blockcmt"
+    INLINECMT = "inlinecmt"
+    
+    # TODO: Error types
+
+
+@dataclass(frozen=True)
+class Token:
+    type: TokenType
+    lexeme: str
+    line: int
+    column: int
+    
+    def to_outlextokens(self) -> str:
+        return f"[{self.type.value}, {self.lexeme}, {self.line}, {self.column}]"
+
+    def to_flaci(self) -> str:
+        # TODO: Determine input format for Flaci tool
+        pass
+
+    def to_outlexerrors(self) -> str:
+        # TODO: Determine what error types to implement. If more than one, implement switch-case functionality to print correct, full text error.
+        pass
